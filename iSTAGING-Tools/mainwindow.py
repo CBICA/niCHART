1e48d9e1feeb4bc7a7054356bf4da9ac7157831f
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-05-21 16:56:43 -0400

    Add basic mainwindow

---
+++ iSTAGING-Tools/mainwindow.py
@@ -0,0 +1,13 @@
+# This Python file uses the following encoding: utf-8
+"""
+contact: software@cbica.upenn.edu
+Copyright (c) 2018 University of Pennsylvania. All rights reserved.
+Use of this source code is governed by license located in license file: https://github.com/CBICA/iSTAGING-Tools/blob/main/LICENSE
+"""
+
+from PyQt5 import QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
