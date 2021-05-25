c1de17572067ef4f7ee61fbe507317019edc1569
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-05-25 18:22:47 -0400

    Add menu, initial interface and hook them up

--- iSTAGING-Tools/mainwindow.py
+++ iSTAGING-Tools/mainwindow.py
@@ -5,9 +5,71 @@ Copyright (c) 2018 University of Pennsylvania. All rights reserved.
 Use of this source code is governed by license located in license file: https://github.com/CBICA/iSTAGING-Tools/blob/main/LICENSE
 """
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.SetupUi()
+
+    def SetupUi(self):
+        self.setObjectName("MainWindow")
+        self.setWindowTitle("MainWindow")
+        self.resize(798, 593)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.setCentralWidget(self.centralwidget)
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        self.label = QtWidgets.QLabel("Plot",self.centralwidget)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+
+        #plot parameters comboBox
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setObjectName("comboBox")
+        self.horizontalLayout_2.addWidget(self.comboBox)
+        self.radioButton = QtWidgets.QRadioButton("Population Average",self.centralwidget)
+        self.radioButton.setObjectName("radioButton")
+        self.horizontalLayout_2.addWidget(self.radioButton)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        #plot widget
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setObjectName("widget")
+        self.verticalLayout.addWidget(self.widget)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton("Ok",self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        #menu bar
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 798, 22))
+        self.menubar.setObjectName("menubar")
+        self.setMenuBar(self.menubar)
+
+        self.menuFile = QtWidgets.QMenu("File",self.menubar)
+        self.menuFile.setObjectName("menuFile")
+
+        self.actionOpen = QtWidgets.QAction("Open",self)
+        self.actionOpen.setObjectName("actionOpen")
+        self.menuFile.addAction(self.actionOpen)
+
+        self.menuProcess = QtWidgets.QMenu("Process",self.menubar)
+        self.menuProcess.setObjectName("menuProcess")
+
+        self.menuHelp = QtWidgets.QMenu("Help",self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuProcess.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
 
