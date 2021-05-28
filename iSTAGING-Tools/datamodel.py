e73d8c711aff55641c6e4a2015a27c9309274d59
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-05-27 21:00:55 -0400

    Add data model class

---
+++ iSTAGING-Tools/datamodel.py
@@ -0,0 +1,26 @@
+# This Python file uses the following encoding: utf-8
+"""
+contact: software@cbica.upenn.edu
+Copyright (c) 2018 University of Pennsylvania. All rights reserved.
+Use of this source code is governed by license located in license file: https://github.com/CBICA/iSTAGING-Tools/blob/main/LICENSE
+"""
+
+import pandas as pd
+import sys
+
+class DataModel:
+    def __init__(self):
+        self.data = None
+
+    def SetData(self,d):
+        self.data = d
+
+    def GetData(self):
+        return self.data
+
+    def IsValid(self):
+        return true
+
+    def GetColumnHeaderNames(self):
+        k = self.data.keys()
+        return k
