e0683fcc2aebdcfc5999ac704cd7c2059a7ca920
Ahmed Abdulkadir <AbdulkaA@pennmedicine.upenn.edu>
2021-06-03 15:57:21 -0400

    Add visualization of normative ranges

--- iSTAGING/mainwindow.py
+++ iSTAGING/mainwindow.py
@@ -9,24 +9,39 @@ from PyQt5 import QtCore, QtWidgets
 #from PyQt5.QtWidgets import QMessageBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from dataio import DataIO
-from datamodel import DataModel
+from iSTAGING.dataio import DataIO
+from iSTAGING.datamodel import DataModel
 import seaborn as sns
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
+    def __init__(self, DataFile=None, HarmonizationModelFile=None):
+        super(MainWindow,self).__init__()
         self.SetupUi()
         self.SetupConnections()
-        self.model = DataModel()
+        self.model = DataModel(DataFile=DataFile,
+                               HarmonizationModelFile=HarmonizationModelFile)
+        if DataFile is not None:
+            #populate the ROI only if the data is valid
+            #Otherwise, show error message
+            if(self.model.IsValid()):
+                self.PopulateROI()
+                self.PopulateHue()
+            else:
+                QtWidgets.QMessageBox.critical(self,
+                'Error',
+                "Invalid Input Data. Please check the data and try again.",
+                QtWidgets.QMessageBox.Ok)
+
 
     def SetupConnections(self):
-        self.actionOpen.triggered.connect(self.OnFileOpenClicked)
+        self.actionOpenDataFile.triggered.connect(self.OnDataFileOpenClicked)
+        self.actionOpenModelFile.triggered.connect(self.OnModelFileOpenClicked)
         self.comboBoxROI.currentIndexChanged.connect(self.UpdatePlot)
+        self.comboBoxHue.currentIndexChanged.connect(self.UpdatePlot)
 
     def SetupUi(self):
         self.setObjectName("MainWindow")
-        self.setWindowTitle("MainWindow")
+        self.setWindowTitle("iSTAGING Data Visualization and Extraction")
         self.resize(798, 593)
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
@@ -34,16 +49,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
 
-        self.label = QtWidgets.QLabel("Plot",self.centralwidget)
-        self.label.setObjectName("label")
-        self.verticalLayout.addWidget(self.label)
-
         #plot parameters comboBox
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.comboBoxROI = QtWidgets.QComboBox(self.centralwidget)
         self.comboBoxROI.setObjectName("comboBoxROI")
         self.horizontalLayout_2.addWidget(self.comboBoxROI)
+        self.comboBoxHue = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBoxHue.setObjectName("comboBoxHue")
+        self.horizontalLayout_2.addWidget(self.comboBoxHue)
         self.radioButton = QtWidgets.QRadioButton("Population Average",self.centralwidget)
         self.radioButton.setObjectName("radioButton")
         self.horizontalLayout_2.addWidget(self.radioButton)
@@ -74,9 +88,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.menuFile = QtWidgets.QMenu("File",self.menubar)
         self.menuFile.setObjectName("menuFile")
 
-        self.actionOpen = QtWidgets.QAction("Open",self)
-        self.actionOpen.setObjectName("actionOpen")
-        self.menuFile.addAction(self.actionOpen)
+        self.actionOpenDataFile = QtWidgets.QAction("Open Data File",self)
+        self.actionOpenDataFile.setObjectName("actionOpen")
+        self.menuFile.addAction(self.actionOpenDataFile)
+
+        self.actionOpenModelFile = QtWidgets.QAction("Open Model File",self)
+        self.actionOpenModelFile.setObjectName("actionOpen")
+        self.menuFile.addAction(self.actionOpenModelFile)
 
         self.menuProcess = QtWidgets.QMenu("Process",self.menubar)
         self.menuProcess.setObjectName("menuProcess")
@@ -89,9 +107,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.menubar.addAction(self.menuHelp.menuAction())
 
 
-    def OnFileOpenClicked(self):
+    def OnDataFileOpenClicked(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(self,
-        'Open file',
+        'Open data file',
         QtCore.QDir().homePath(),
         "Pickle files (*.pkl.gz)")
 
@@ -109,6 +127,35 @@ class MainWindow(QtWidgets.QMainWindow):
         #Otherwise, show error message
         if(self.model.IsValid()):
             self.PopulateROI()
+            self.PopulateHue()
+        else:
+            QtWidgets.QMessageBox.critical(self,
+            'Error',
+            "Invalid Input Data. Please check the data and try again.",
+            QtWidgets.QMessageBox.Ok)
+
+
+    def OnModelFileOpenClicked(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(self,
+        'Open iSTAGING model file',
+        QtCore.QDir().homePath(),
+        "Pickle files (*.pkl)")
+
+        if not filename[0]:
+            return
+
+        #read input data
+        dio = DataIO()
+        m = dio.ReadPickleFile(filename[0])
+
+        #set data in model
+        self.model.SetHarmonizationModel(m)
+
+        #populate the ROI only if the data is valid
+        #Otherwise, show error message
+        if(self.model.IsValid()):
+            self.PopulateROI()
+            self.PopulateHue()
         else:
             QtWidgets.QMessageBox.critical(self,
             'Error',
@@ -120,6 +167,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
         #get current selected combobox item
         currentROI = self.comboBoxROI.currentText()
+        currentHue = self.comboBoxHue.currentText()
+        if not currentHue:
+            currentHue = 'Sex'
 
         # clear plot
         self.figure.clear()
@@ -127,8 +177,18 @@ class MainWindow(QtWidgets.QMainWindow):
         # create an axis
         ax = self.figure.add_subplot(111)
 
-        #seaborn plot on axis
-        sns.scatterplot(x='Age', y=currentROI, data=self.model.GetData(currentROI,"Sex"), hue='Sex',ax=ax)
+        # seaborn plot on axis
+        sns.scatterplot(x='Age', y=currentROI,  hue=currentHue,ax=ax, s=5,
+                       data=self.model.GetData(currentROI,currentHue))
+        
+        # Plot normative range if according GAM model is available
+        if (self.model.harmonization_model is not None) and (currentROI in self.model.harmonization_model['ROIs']):
+            x,y,z = self.model.GetNormativeRange(currentROI)
+            print('Pooled variance: %f' % (z))
+            # Plot three lines as expected mean and +/- 2 times standard deviation
+            sns.lineplot(x=x, y=y, ax=ax, linestyle='-', markers=False, color='k')
+            sns.lineplot(x=x, y=y+z, ax=ax, linestyle=':', markers=False, color='k')
+            sns.lineplot(x=x, y=y-z, ax=ax, linestyle=':', markers=False, color='k')
 
         # refresh canvas
         self.canvas.draw()
@@ -138,8 +198,21 @@ class MainWindow(QtWidgets.QMainWindow):
         datakeys = self.model.GetColumnHeaderNames()
 
         #construct ROI list to populate comboBox
-        roiList = [x for x in datakeys if x.startswith('MUSE_Volume')]
+        roiList = (  [x for x in datakeys if x.startswith('MUSE_Volume')]
+                   + [x for x in datakeys if x.startswith('H_MUSE_Volume')]
+                   + [x for x in datakeys if x.startswith('WMLS_Volume')]
+                   + [x for x in datakeys if x.startswith('H_WMLS_Volume')]
+                   + ['SPARE_AD','SPARE_BA','Non-existing-ROI','DLICV'])
+        roiList = list(set(roiList).intersection(set(datakeys)))
+        roiList.sort()
 
         #add the list items to comboBox
         self.comboBoxROI.addItems(roiList)
 
+
+    def PopulateHue(self):
+        #add the list items to comboBoxHue
+        datakeys = self.model.GetColumnHeaderNames()
+        categoryList = ['Sex','Study','A','T','N','PIB_Status']
+        categoryList = list(set(categoryList).intersection(set(datakeys)))
+        self.comboBoxHue.addItems(categoryList)
