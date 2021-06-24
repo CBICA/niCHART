cc34d7edac95a310d4e83a5a029d90a03bdf3965
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-06-24 17:05:41 -0400

    Add boilerplate processes

---
+++ iSTAGING/processes.py
@@ -0,0 +1,25 @@
+# This Python file uses the following encoding: utf-8
+"""
+contact: software@cbica.upenn.edu
+Copyright (c) 2018 University of Pennsylvania. All rights reserved.
+Use of this source code is governed by license located in license file: https://github.com/CBICA/iSTAGING-Tools/blob/main/LICENSE
+"""
+
+import pandas as pd
+import sys
+
+class Processes:
+    def __init__(self):
+        pass
+
+    def DoSpare(self,data):
+        #TODO: implement functionality
+        pass
+        #return modifieddata
+
+    def DoHarmonization(self,data):
+        #TODO: implement functionality
+        pass
+        #return modifieddata
+
+
