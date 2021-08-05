1aa7e09cc8a491ffdd63751695a52f30d69ce9eb
Ahmed Abdulkadir <ahmed.abdulkadir@upd.unibe.ch>
2021-08-05 16:21:32 -0400

    Translate MUSE ID into MUSE name and vice-versa
    Fix #41

--- iSTAGING/mainwindow.py
+++ iSTAGING/mainwindow.py
@@ -12,7 +12,7 @@ from matplotlib.figure import Figure
 from iSTAGING.dataio import DataIO
 from iSTAGING.datamodel import DataModel
 from iSTAGING.plotcanvas import PlotCanvas
-import seaborn as sns
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, dataFile=None, harmonizationModelFile=None):
@@ -24,6 +24,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.currentView = 'AgeTrend'
         #Instantiate data model
         self.model = DataModel()
+
+        dio = DataIO()
+        MUSEDictNAMEtoID, MUSEDictIDtoNAME = dio.ReadMUSEDictionary()
+        self.model.SetMUSEDictionaries(MUSEDictNAMEtoID, MUSEDictIDtoNAME)
 
         if dataFile is not None:
             # if datafile provided on cmd line, load it
@@ -278,6 +282,19 @@ class MainWindow(QtWidgets.QMainWindow):
         currentROI = self.comboBoxROI.currentText()
         currentHue = self.comboBoxHue.currentText()
 
+
+        # Translate ROI name back to ROI ID
+        MUSEDictNAMEtoID, _ = self.model.GetMUSEDictionaries()
+        if currentROI.startswith('(MUSE)'):
+            currentROI = list(map(MUSEDictNAMEtoID.get, [currentROI[7:]]))[0]
+
+        if currentROI.startswith('(Harmonized MUSE)'):
+            currentROI = 'H_' + list(map(MUSEDictNAMEtoID.get, [currentROI[18:]]))[0]
+
+        if currentROI.startswith('(WMLS)'):
+            currentROI = list(map(MUSEDictNAMEtoID.get, [currentROI[7:]]))[0].replace('MUSE_', 'WMLS_')
+
+
         #create empty dictionary of plot options
         plotOptions = dict()
 
@@ -299,8 +316,24 @@ class MainWindow(QtWidgets.QMainWindow):
                    + [x for x in datakeys if x.startswith('WMLS_Volume')]
                    + [x for x in datakeys if x.startswith('H_WMLS_Volume')]
                    + ['SPARE_AD','SPARE_BA','Non-existing-ROI','DLICV'])
+        
+        # !!! remove ROI with no dictionary entry
+        if 'WMLS_Volume_43' in roiList:
+            roiList.remove('WMLS_Volume_43')
+
+        if 'WMLS_Volume_42' in roiList:
+            roiList.remove('WMLS_Volume_42')
+        
+        if 'WMLS_Volume_69' in roiList:
+            roiList.remove('WMLS_Volume_69')
+
+
+        _, MUSEDictIDtoNAME = self.model.GetMUSEDictionaries()
         roiList = list(set(roiList).intersection(set(datakeys)))
         roiList.sort()
+        roiList = ['(MUSE) ' + list(map(MUSEDictIDtoNAME.get, [k]))[0] if k.startswith('MUSE_') else k for k in roiList]
+        roiList = ['(Harmonized MUSE) ' + list(map(MUSEDictIDtoNAME.get, [k[2:]]))[0] if k.startswith('H_MUSE_') else k for k in roiList]
+        roiList = ['(WMLS) ' + list(map(MUSEDictIDtoNAME.get, [k.replace('WMLS_', 'MUSE_')]))[0] if k.startswith('WMLS_') else k for k in roiList]
 
         #add the list items to comboBox
         self.comboBoxROI.addItems(roiList)
