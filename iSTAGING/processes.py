dc8fb5687b178c7d6dac2d8d3984ce5917ed56e5
Ahmed Abdulkadir <ahmed.abdulkadir@upd.unibe.ch>
2021-09-09 13:25:45 -0400

    Predictions functions re-defined as member functions

--- iSTAGING/processes.py
+++ iSTAGING/processes.py
@@ -11,64 +11,66 @@ import sys
 import neuroHarmonize as nh
 
 
-def predictBrainAge(data,model):
-    
-    idx = ~data[model['predictors'][0]].isnull()
-
-    y_hat_test = np.zeros((np.sum(idx),))
-    n_ensembles = np.zeros((np.sum(idx),))
-
-    for i,_ in enumerate(model['scaler']):
-        print('SPARE-BA fold %d' % (i))
-        # Predict validation (fold) and test
-        test = np.logical_not(data[idx]['participant_id'].isin(np.concatenate(model['train']))) | data[idx]['participant_id'].isin(model['validation'][i])
-        X = data[idx].loc[test, model['predictors']].values
-        X = model['scaler'][i].transform(X)
-        y_hat_test[test] += (model['svm'][i].predict(X) - model['bias_ints'][i]) / model['bias_slopes'][i]
-        n_ensembles[test] += 1.
-
-    y_hat_test /= n_ensembles
-    y_hat = np.full((data.shape[0],),np.nan)
-    y_hat[idx] = y_hat_test
-
-    return y_hat
-
-
-def predictAD(data,model):
-    
-    idx = ~data[model['predictors'][0]].isnull()
-
-    y_hat_test = np.zeros((np.sum(idx),))
-    n_ensembles = np.zeros((np.sum(idx),))
-
-    for i,_ in enumerate(model['scaler']):
-        print('SPARE-AD fold %d' % (i))
-        # Predict validation (fold) and test
-        test = np.logical_not(data[idx]['participant_id'].isin(np.concatenate(model['train']))) | data[idx]['participant_id'].isin(model['validation'][i])
-        X = data[idx].loc[test, model['predictors']].values
-        X = model['scaler'][i].transform(X)
-        y_hat_test[test] += model['svm'][i].decision_function(X)
-        n_ensembles[test] += 1.
-
-    y_hat_test /= n_ensembles
-    y_hat = np.full((data.shape[0],),np.nan)
-    y_hat[idx] = y_hat_test
-
-    return y_hat
-
 
 class Processes:
     def __init__(self):
         pass
 
+    def predictBrainAge(self, data, model):
+        
+        idx = ~data[model['predictors'][0]].isnull()
+
+        y_hat_test = np.zeros((np.sum(idx),))
+        n_ensembles = np.zeros((np.sum(idx),))
+
+        for i,_ in enumerate(model['scaler']):
+            print('SPARE-BA fold %d' % (i))
+            # Predict validation (fold) and test
+            test = np.logical_not(data[idx]['participant_id'].isin(np.concatenate(model['train']))) | data[idx]['participant_id'].isin(model['validation'][i])
+            X = data[idx].loc[test, model['predictors']].values
+            X = model['scaler'][i].transform(X)
+            y_hat_test[test] += (model['svm'][i].predict(X) - model['bias_ints'][i]) / model['bias_slopes'][i]
+            n_ensembles[test] += 1.
+
+        y_hat_test /= n_ensembles
+        y_hat = np.full((data.shape[0],),np.nan)
+        y_hat[idx] = y_hat_test
+
+        return y_hat
+
+
+    def predictAD(self, data, model):
+        
+        idx = ~data[model['predictors'][0]].isnull()
+
+        y_hat_test = np.zeros((np.sum(idx),))
+        n_ensembles = np.zeros((np.sum(idx),))
+
+        for i,_ in enumerate(model['scaler']):
+            print('SPARE-AD fold %d' % (i))
+            # Predict validation (fold) and test
+            test = np.logical_not(data[idx]['participant_id'].isin(np.concatenate(model['train']))) | data[idx]['participant_id'].isin(model['validation'][i])
+            X = data[idx].loc[test, model['predictors']].values
+            X = model['scaler'][i].transform(X)
+            y_hat_test[test] += model['svm'][i].decision_function(X)
+            n_ensembles[test] += 1.
+
+        y_hat_test /= n_ensembles
+        y_hat = np.full((data.shape[0],),np.nan)
+        y_hat[idx] = y_hat_test
+
+        return y_hat
+
+
     def DoSPARE(self,data, ADModel, BrainAgeModel):
         print('Computing SPARE-*.')
-        y_hat = predictAD(data, ADModel)
+        y_hat = self.predictAD(data, ADModel)
         data['SPARE_AD'] = y_hat
-        y_hat = predictBrainAge(data, BrainAgeModel)
+        y_hat = self.predictBrainAge(data, BrainAgeModel)
         data['SPARE_BA'] = y_hat
         print('Computing SPARE-* done.')
         return data
+
 
     def DoHarmonization(self, data, model):
         print('Running harmonization.')
