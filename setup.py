88e506d5b86fc0c4a287d73ed2dff4c3c90218ae
Ahmed Abdulkadir <ahmed.abdulkadir@upd.unibe.ch>
2021-09-11 14:43:22 -0400

    Add name and version to package setup

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 import setuptools
 
-setuptools.setup()
+setuptools.setup( name="iSTAGING",
+                  version="0.0.1",
+                  packages=setuptools.find_packages(),
+                  include_package_data=True,
+                  package_data = {"iSTAGING": ['MUSE_ROI_Dictionary.csv']},
+                  )
