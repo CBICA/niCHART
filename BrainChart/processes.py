e7771d52f70f754c71fd3345f3f3f050e6210489
melhemr <randa.melhem@pennmedicine.upenn.edu>
2021-10-07 13:52:33 -0400

    Add out-of-sample harmonization for new sites

--- BrainChart/processes.py
+++ BrainChart/processes.py
@@ -73,14 +73,40 @@ class Processes:
 
     def DoHarmonization(self, data, model):
         print('Running harmonization.')
+
+
         data['Sex'] = data['Sex'].map({'M':1,'F':0})
         bayes_data, stand_mean = nh.harmonizationApply(data[[x for x in model['ROIs']]].values,
                                                 data[['SITE','Age','Sex','DLICV_baseline']],
                                                 model,True)
+
+        Raw_ROIs_Residuals = data[model['ROIs']].values - stand_mean
+
+        # create list of new SITEs to loop through
+        new_sites = set(data['SITE'].value_counts().index.tolist())^set(model['SITE_labels'])
+
+        var_pooled = model['var_pooled']
+
+        for site in new_sites:
+            missing = np.array(data['SITE']==site,dtype=bool)
+            training = np.array(data['UseForComBatGAMHarmonization'],dtype=bool)
+            new_site_is_train = np.logical_and(missing, training)
+
+            if np.count_nonzero(new_site_is_train)<25:
+                print('New site `'+site+'` has less than 25 reference data points. Skipping harmonization.')
+                continue
+
+            gamma_hat_site = np.mean(((Raw_ROIs_Residuals[new_site_is_train,:])/np.dot(np.sqrt(var_pooled),np.ones((1,Raw_ROIs_Residuals[new_site_is_train,:].shape[0]))).T),0)
+            gamma_hat_site = gamma_hat_site[:,np.newaxis]
+            delta_hat_site = pow(np.std(((Raw_ROIs_Residuals[new_site_is_train,:])/np.dot(np.sqrt(var_pooled),np.ones((1,Raw_ROIs_Residuals[new_site_is_train,:].shape[0]))).T),0),2)
+            delta_hat_site = delta_hat_site[:,np.newaxis]
+
+            bayes_data[missing,:] = ((Raw_ROIs_Residuals[missing,:]/np.dot(np.sqrt(var_pooled),np.ones((1,Raw_ROIs_Residuals[missing,:].shape[0]))).T) - np.dot(gamma_hat_site,np.ones((1,Raw_ROIs_Residuals[missing,:].shape[0]))).T)*np.dot(np.sqrt(var_pooled),np.ones((1,Raw_ROIs_Residuals[missing,:].shape[0]))).T/np.dot(np.sqrt(delta_hat_site),np.ones((1,Raw_ROIs_Residuals[missing,:].shape[0]))).T + stand_mean[missing,:]
+
+
         if ('H_MUSE_Volume_47' not in data.keys()):
             data = pd.concat([data.reset_index(), pd.DataFrame(bayes_data, columns=['H_' + s for s in model['ROIs']])],
-                            axis=1)
-        
+                            axis=1)    
         start_index = len(model['SITE_labels'])
         sex_icv_effect = np.dot(data[['Sex','DLICV_baseline']],model['B_hat'][start_index:(start_index+2),:])
         ROIs_ICV_Sex_Residuals = ['RES_ICV_Sex_' + x for x in model['ROIs']]
