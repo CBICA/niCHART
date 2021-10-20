405d62fe7154cdbcc185abfe44b7507bd3711195
Abdulkadir, Ahmed <ahmed.abdulkadir@pennmedicine.upenn.edu>
2021-10-20 08:18:42 -0400

    Make ROI combo box searchable

--- BrainChart/mainwindow.py
+++ BrainChart/mainwindow.py
@@ -17,6 +17,51 @@ import BrainChart.processes
 import struct
 import pickle
 
+class ExtendedComboBox(QtWidgets.QComboBox):
+    def __init__(self, parent=None):
+        super(ExtendedComboBox, self).__init__(parent)
+
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.setEditable(True)
+
+        # add a filter model to filter matching items
+        self.pFilterModel = QtCore.QSortFilterProxyModel(self)
+        self.pFilterModel.setFilterCaseSensitivity(QtCore.Qt.CaseInsensitive)
+        self.pFilterModel.setSourceModel(self.model())
+
+        # add a completer, which uses the filter model
+        self.completer = QtWidgets.QCompleter(self.pFilterModel, self)
+        # always show all (filtered) completions
+        self.completer.setCompletionMode(QtWidgets.QCompleter.UnfilteredPopupCompletion)
+        self.setCompleter(self.completer)
+
+        # connect signals
+        self.lineEdit().textEdited.connect(self.pFilterModel.setFilterFixedString)
+        self.completer.activated.connect(self.on_completer_activated)
+
+
+    # on selection of an item from the completer, select the corresponding item from combobox 
+    def on_completer_activated(self, text):
+        if text:
+            index = self.findText(text)
+            self.setCurrentIndex(index)
+            self.activated[str].emit(self.itemText(index))
+
+
+    # on model change, update the models of the filter and completer as well 
+    def setModel(self, model):
+        super(ExtendedComboBox, self).setModel(model)
+        self.pFilterModel.setSourceModel(model)
+        self.completer.setModel(self.pFilterModel)
+
+
+    # on model column change, update the model column of the filter and completer as well
+    def setModelColumn(self, column):
+        self.completer.setCompletionColumn(column)
+        self.pFilterModel.setFilterKeyColumn(column)
+        super(ExtendedComboBox, self).setModelColumn(column)    
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, dataFile=None, harmonizationModelFile=None, SPAREModelFile=None):
         super(MainWindow,self).__init__()
@@ -147,7 +192,7 @@ class MainWindow(QtWidgets.QMainWindow):
         #plot parameters comboBox
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.comboBoxROI = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBoxROI = ExtendedComboBox(self.centralwidget)
         self.comboBoxROI.setObjectName("comboBoxROI")
         self.horizontalLayout_2.addWidget(self.comboBoxROI)
         self.comboBoxHue = QtWidgets.QComboBox(self.centralwidget)
