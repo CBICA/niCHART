06385bd2df1b78b90b4faf8843f01d37f7b54f97
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-11-18 22:19:29 -0500

    Add age trends plugin, move plotcanvas to core modules

--- QtBrainChartGUI/plugins/computeSPAREs/computeSPAREs.py
+++ QtBrainChartGUI/plugins/computeSPAREs/computeSPAREs.py
@@ -8,32 +8,7 @@ import sys, os
 import seaborn as sns
 import numpy as np
 import pandas as pd
-
-# Plotting with matplotlib
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-import matplotlib as mpl
-mpl.use('QT5Agg')
-
-
-class PlotCanvas(FigureCanvas):
-    """ A generic Plotting class that derives from FigureCanvasQTAgg
-    and plots data as per different options"""
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        """The constructor."""
-
-        # a figure instance to plot on
-        fig = mpl.figure.Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-
-        #super clas
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                QtWidgets.QSizePolicy.Expanding,
-                QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+from QtBrainChartGUI.core.plotcanvas import PlotCanvas
 
 class computeSPAREs(QtWidgets.QWidget,IPlugin):
 
@@ -45,9 +20,8 @@ class computeSPAREs(QtWidgets.QWidget,IPlugin):
         self.ui = uic.loadUi(os.path.join(root, 'computeSPAREs.ui'),self)
         self.plotCanvas = PlotCanvas(self.ui.page_2)
         self.ui.verticalLayout.addWidget(self.plotCanvas)
+        self.plotCanvas.axes = self.plotCanvas.fig.add_subplot(111)
         self.SPAREs = None
-        #TODO: hook up plot functionality
-
         self.ui.stackedWidget.setCurrentIndex(0)
 
 
