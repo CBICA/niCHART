ac87ab48700861ac3bf02f1f3304eef6208ebffc
Ahmed Abdulkadir <ahmed.abdulkadir@pennmedicine.upenn.edu>
2021-11-23 18:40:54 -0500

    Improve SPARE-* plot

--- QtBrainChartGUI/plugins/computeSPAREs/computeSPAREs.py
+++ QtBrainChartGUI/plugins/computeSPAREs/computeSPAREs.py
@@ -3,7 +3,7 @@ from matplotlib.backends.backend_qt5 import FigureCanvasQT
 from yapsy.IPlugin import IPlugin
 from PyQt5 import QtGui, QtCore, QtWidgets, uic
 import joblib
-import sys, os
+import sys, os, time
 
 import seaborn as sns
 import numpy as np
@@ -23,6 +23,7 @@ class computeSPAREs(QtWidgets.QWidget,IPlugin):
         self.plotCanvas.axes = self.plotCanvas.fig.add_subplot(111)
         self.SPAREs = None
         self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.factorial_progressBar.setValue(0)
 
 
     def getUI(self):
@@ -43,17 +44,20 @@ class computeSPAREs(QtWidgets.QWidget,IPlugin):
         # are present in data frame
         if ('SPARE_BA' in self.datamodel.GetColumnHeaderNames() and
             'SPARE_AD' in self.datamodel.GetColumnHeaderNames()):
+            self.ui.show_SPARE_scores_from_data_Btn.setStyleSheet("background-color: rgb(230,230,255)")
             self.ui.show_SPARE_scores_from_data_Btn.setEnabled(True)
+            self.ui.show_SPARE_scoresfrom_data_Btn.setToolTip('The data frame has variables `SPARE_AD` and `SPARE_BA` so these can be plotted.')
         else:
             self.ui.show_SPARE_scores_from_data_Btn.setEnabled(False)
 
-        # Allow loading of SPARE-* model only when harmonized residuals are
-        # present
-        if 'RES_ICV_Sex_MUSE_Volume_47' in self.datamodel.GetColumnHeaderNames():
-            self.ui.load_SPARE_model_Btn.setEnabled(True)
-        else:
-            self.ui.load_SPARE_model_Btn.setEnabled(False)
+        # Allow loading of SPARE-* model always, even when residuals are not
+        # calculated yet
+        self.ui.load_SPARE_model_Btn.setEnabled(True)
 
+
+    def updateProgress(self, txt, vl):
+        self.ui.SPARE_computation_info.setText(txt)
+        self.ui.factorial_progressBar.setValue(vl)
 
 
     def OnLoadSPAREModel(self):
@@ -64,25 +68,56 @@ class computeSPAREs(QtWidgets.QWidget,IPlugin):
         if fileName != "":
             self.model['BrainAge'], self.model['AD'] = joblib.load(fileName)
             self.ui.compute_SPARE_scores_Btn.setEnabled(True)
+            self.ui.SPARE_model_info.setText('File: %s' % (fileName))
         
         self.ui.stackedWidget.setCurrentIndex(0)
 
+        if 'RES_ICV_Sex_MUSE_Volume_47' in self.datamodel.GetColumnHeaderNames():
+            self.ui.compute_SPARE_scores_Btn.setStyleSheet("background-color: rgb(230,255,230)")
+            self.ui.compute_SPARE_scores_Btn.setEnabled(True)
+            self.ui.compute_SPARE_scores_Btn.setToolTip('Model loaded and `RES_ICV_Sex_MUSE_Volmue_*` available so the MUSE volumes can be harmonized.')
+        else:
+            self.ui.compute_SPARE_scores_Btn.setStyleSheet("background-color: rgb(255,230,230)")
+            self.ui.compute_SPARE_scores_Btn.setEnabled(False)
+            self.ui.compute_SPARE_scores_Btn.setToolTip('Model loaded but `RES_ICV_Sex_MUSE_Volmue_*` not available so the MUSE volumes can not be harmonized.')
+
+
+            print('No field `RES_ICV_Sex_MUSE_Volume_47` found. ' +
+                  'Make sure to compute and add harmonized residuals first.')
+
+
+    def OnComputationDone(self, y_hat):
+        self.SPAREs = y_hat
+        self.plotSPAREs()
+        self.ui.stackedWidget.setCurrentIndex(1)
+        
+
 
     def OnComputeSPAREs(self):
-        if 'RES_ICV_Sex_MUSE_Volume_47' in self.datamodel.GetColumnHeaderNames():
-            self.SPAREs = pd.DataFrame.from_dict({
-                'SPARE_BA': predictBrainAge(self.datamodel.data, self.model['BrainAge']),
-                'SPARE_AD': predictAD(self.datamodel.data, self.model['AD'])})
-            self.plotSPAREs()
-            self.ui.stackedWidget.setCurrentIndex(1)
-        else:
-            print('No field `RES_ICV_Sex_MUSE_Volume_47` found. ' +
-                  'Make sure to compute harmonized residuals first.')
+        # Setup tasks for long running jobs
+        # Using this example: https://realpython.com/python-pyqt-qthread/
+        self.thread = QtCore.QThread()
+        self.worker = BrainAgeWorker(self.datamodel.data, self.model)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.done.connect(self.thread.quit)
+        self.worker.done.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.worker.progress.connect(self.updateProgress)
+        self.worker.done.connect(lambda y_hat: self.OnComputationDone(y_hat))
+        self.ui.factorial_progressBar.setRange(0, len(self.model['BrainAge']['scaler'])-1)
+        self.thread.start()
+        self.ui.compute_SPARE_scores_Btn.setEnabled(False)
 
 
     def plotSPAREs(self):
+        # Plot data
         sns.scatterplot(x='SPARE_AD', y='SPARE_BA', data=self.SPAREs,
-                        ax=self.plotCanvas.axes)
+                        ax=self.plotCanvas.axes, linewidth=0,
+                        facecolor=(0.5, 0.5, 0.5, 0.5), size=1, legend=None)
+        sns.despine(ax=self.plotCanvas.axes, trim=True)
+        self.plotCanvas.axes.set(ylabel='SPARE-BA', xlabel='SPARE-AD')
+        self.plotCanvas.axes.get_figure().set_tight_layout(True)
 
 
     def OnAddToDataFrame(self):
@@ -103,58 +138,63 @@ class computeSPAREs(QtWidgets.QWidget,IPlugin):
         if ('SPARE_BA' in self.datamodel.GetColumnHeaderNames() and
             'SPARE_AD' in self.datamodel.GetColumnHeaderNames()):
             self.ui.show_SPARE_scores_from_data_Btn.setEnabled(True)
+            self.ui.show_SPARE_scores_from_data_Btn.setStyleSheet("background-color: rgb(230,230,255)")
         else:
             self.ui.show_SPARE_scores_from_data_Btn.setEnabled(False)
 
-        # Allow loading of SPARE-* model only when harmonized residuals are
-        # present
-        if 'RES_ICV_Sex_MUSE_Volume_47' in self.datamodel.GetColumnHeaderNames():
-            self.ui.load_SPARE_model_Btn.setEnabled(True)
-        else:
-            self.ui.load_SPARE_model_Btn.setEnabled(False)
 
+class BrainAgeWorker(QtCore.QObject):
 
-def predictBrainAge(data,model):
-    
-    idx = ~data[model['predictors'][0]].isnull()
+    done = QtCore.pyqtSignal(pd.DataFrame)
+    progress = QtCore.pyqtSignal(str, int)
 
-    y_hat_test = np.zeros((np.sum(idx),))
-    n_ensembles = np.zeros((np.sum(idx),))
+    #constructor
+    def __init__(self, data, model):
+        super(BrainAgeWorker, self).__init__()
+        self.data = data
+        self.model = model
 
-    for i,_ in enumerate(model['scaler']):
-        # Predict validation (fold) and test
-        print('Fold %d' % (i))
-        test = np.logical_not(data[idx]['participant_id'].isin(np.concatenate(model['train']))) | data[idx]['participant_id'].isin(model['validation'][i])
-        X = data[idx].loc[test, model['predictors']].values
-        X = model['scaler'][i].transform(X)
-        y_hat_test[test] += (model['svm'][i].predict(X) - model['bias_ints'][i]) / model['bias_slopes'][i]
-        n_ensembles[test] += 1.
+    def run(self):
+        y_hat = pd.DataFrame.from_dict({'SPARE_BA': np.full((self.data.shape[0],),np.nan),
+                                       'SPARE_AD': np.full((self.data.shape[0],),np.nan)})
 
-    y_hat_test /= n_ensembles
-    y_hat = np.full((data.shape[0],),np.nan)
-    y_hat[idx] = y_hat_test
+        # SPARE-BA
+        idx = ~self.data[self.model['BrainAge']['predictors'][0]].isnull()
 
-    return y_hat
+        y_hat_test = np.zeros((np.sum(idx),))
+        n_ensembles = np.zeros((np.sum(idx),))
 
+        for i,_ in enumerate(self.model['BrainAge']['scaler']):
+            # Predict validation (fold) and test
+            self.progress.emit('Computing SPARE-BA | Task 1 of 2', i)
+            test = np.logical_not(self.data[idx]['participant_id'].isin(np.concatenate(self.model['BrainAge']['train']))) | self.data[idx]['participant_id'].isin(self.model['BrainAge']['validation'][i])
+            X = self.data[idx].loc[test, self.model['BrainAge']['predictors']].values
+            X = self.model['BrainAge']['scaler'][i].transform(X)
+            y_hat_test[test] += (self.model['BrainAge']['svm'][i].predict(X) - self.model['BrainAge']['bias_ints'][i]) / self.model['BrainAge']['bias_slopes'][i]
+            n_ensembles[test] += 1.
 
-def predictAD(data,model):
-    
-    idx = ~data[model['predictors'][0]].isnull()
+        y_hat_test /= n_ensembles
+        y_hat.loc[idx, 'SPARE_BA'] = y_hat_test
 
-    y_hat_test = np.zeros((np.sum(idx),))
-    n_ensembles = np.zeros((np.sum(idx),))
+        idx = ~self.data[self.model['BrainAge']['predictors'][0]].isnull()
 
-    for i,_ in enumerate(model['scaler']):
-        # Predict validation (fold) and test
-        print('Fold %d' % (i))
-        test = np.logical_not(data[idx]['participant_id'].isin(np.concatenate(model['train']))) | data[idx]['participant_id'].isin(model['validation'][i])
-        X = data[idx].loc[test, model['predictors']].values
-        X = model['scaler'][i].transform(X)
-        y_hat_test[test] += model['svm'][i].decision_function(X)
-        n_ensembles[test] += 1.
+        y_hat_test = np.zeros((np.sum(idx),))
+        n_ensembles = np.zeros((np.sum(idx),))
 
-    y_hat_test /= n_ensembles
-    y_hat = np.full((data.shape[0],),np.nan)
-    y_hat[idx] = y_hat_test
+        for i,_ in enumerate(self.model['AD']['scaler']):
+            # Predict validation (fold) and test
+            self.progress.emit('Computing SPARE-AD | Task 2 of 2', i)
 
-    return y_hat
+            test = np.logical_not(self.data[idx]['participant_id'].isin(np.concatenate(self.model['AD']['train']))) | self.data[idx]['participant_id'].isin(self.model['AD']['validation'][i])
+            X = self.data[idx].loc[test, self.model['AD']['predictors']].values
+            X = self.model['AD']['scaler'][i].transform(X)
+            y_hat_test[test] += self.model['AD']['svm'][i].decision_function(X)
+            n_ensembles[test] += 1. 
+
+        y_hat_test /= n_ensembles
+        y_hat.loc[idx, 'SPARE_AD'] = y_hat_test
+
+        self.progress.emit('All done.', i)
+
+        # Emit the result
+        self.done.emit(y_hat)
