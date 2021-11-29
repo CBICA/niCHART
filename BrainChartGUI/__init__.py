04f76e65a939558be69c715f6b8101fc69ae031b
Abdulkadir, Ahmed <ahmed.abdulkadir@pennmedicine.upenn.edu>
2021-11-29 11:58:16 -0500

    Remove Pyguitemp implementation

--- BrainChartGUI/__init__.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from pyguitemp.logging import logger
-
-VERSION = "0.0.1"
-APP_NAME = Path(__file__).parent.stem
-logger.app_name = APP_NAME
--- BrainChartGUI/__main__.py
+++
@@ -1,7 +0,0 @@
-"""
-The entry point of the program. It launches the main application - i.e. it starts
-whatever represents the mainloop in the chosen GUI toolkit.
-"""
-from pyguitemp.scripts import run
-
-run()
