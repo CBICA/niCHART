f1462c9c930b92bc4843fd743ed8a3bf0bbe0460
Ashish Singh <ashish.singh@uphs.upenn.edu>
2022-03-08 20:19:58 -0500

    Add test for PlotCanvas

---
+++ testing/test_plotcanvas.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+from PyQt5 import QtCore, QtWidgets
+from NiBAx.core.plotcanvas import PlotCanvas
+import matplotlib
+
+#This file contains tests for PlotCanvas.py
+
+def test_init_PlotCanvas(qtbot):
+
+    #instantiate PlotCanvas class to be tested
+    plotCanvas = PlotCanvas()
+
+    #read expected data
+    fig = plotCanvas.getFigure()
+
+    #compare read width and height of figure to default values from PlotCanvas constructor
+    if (fig.get_figheight() == 4.0 and fig.get_figwidth() == 5.0):
+        assert True
+    else:
+        assert False
+
+
